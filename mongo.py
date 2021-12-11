65557f5449a34d46c45478a45223274a76be1cc4
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-12-11 14:02:09 -0600

    Making charts n stuff

--- mongo.py
+++ mongo.py
@@ -2,12 +2,13 @@ import pandas as pd
 import os
 import pymongo
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
 db = client["project2"]
-
-for x in os.listdir("../this_one"):
+for x in os.listdir("this_one"):
     df = pd.read_csv(f'this_one/{x}')
     x = x.replace(" ", "_")[:len(x)-4]
     collist = db.list_collection_names()
@@ -15,3 +16,45 @@ for x in os.listdir("../this_one"):
     if x not in collist:
         db[x].insert_many(records)
 
+for x in db.list_collection_names():
+    df = pd.DataFrame(list(db[x].find()))
+    del df['_id']
+# print(db.list_collection_names())
+# data = list(db["texas_combined_completed_data"].find())
+# print(json.dumps(data, default=str, indent=4))
+df = pd.DataFrame(list(db["texas_combined_completed_data"].find()))
+del df['_id']
+
+print(df.columns)
+death_rate = []
+vaccinated_rate = []
+for x in range(len(df["Deaths"]) - 1):
+    death_rate.append(float(df["Deaths"].tolist()[x + 1] - df["Deaths"].tolist()[x]))
+    vaccinated_rate.append(df["People_Fully_Vaccinated"].tolist()[x + 1] - df["People_Fully_Vaccinated"].tolist()[x])
+print(death_rate)
+print(vaccinated_rate)
+
+print(len(df))
+# plt.bar(death_rate, vaccinated_rate)
+# plt.show()
+dr_monthly = []
+vr_monthly = []
+for x in range(0, 273, 7):
+    dr = death_rate[x]
+    vr = vaccinated_rate[x]
+    for y in range(1, 7):
+        dr += death_rate[x+y]
+        vr += vaccinated_rate[x+y]
+    dr_monthly.append(dr/7)
+    vr_monthly.append(vr/7/100)
+print(dr_monthly)
+print(vr_monthly)
+plt.plot(
+    np.arange((len(vr_monthly))),
+    vr_monthly
+)
+plt.plot(
+    np.arange(len(dr_monthly)),
+    dr_monthly
+)
+plt.show()
--- sql.py
+++ sql.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, desc
 
 engine = sqlalchemy.create_engine('sqlite://', echo=False)
-for x in os.listdir("../this_one"):
+for x in os.listdir("this_one"):
     df = pd.read_csv(f'this_one/{x}')
     x = x.replace(" ", "_")[:len(x)-4]
     df.to_sql(x, con=engine)
